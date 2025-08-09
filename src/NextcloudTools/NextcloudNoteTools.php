4505f83d1be4074d0cc97e57535ae66fdf02ef59
Julian Drauz <julian@drauz-mail.de>
2025-08-09 14:17:28 +0200

    Refactor tools

--- src/NextcloudTools/NextcloudNoteTools.php
+++ src/NextcloudTools/NextcloudNoteTools.php
@@ -6,16 +6,17 @@ use Symfony\AI\Agent\Toolbox\Attribute\AsTool;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-#[AsTool('nextcloud-list-notes', description: 'Lists all notes.')]
+#[AsTool('nextcloud-list-notes', description: 'Lists all notes.', method: 'list')]
+#[AsTool('nextcloud-read-note', description: 'Read a note by its ID', method: 'read')]
 #[AutoconfigureTag('symfony.ai.agent.toolbox.tool')]
-class ListNotes
+class NextcloudNoteTools
 {
     public function __construct(
         private HttpClientInterface $nextcloudClient,
     ) {
     }
 
-    public function __invoke(): string
+    public function list(): string
     {
         $response = $this->nextcloudClient->request(
             'GET',
@@ -33,5 +34,20 @@ class ListNotes
             fn($note) => '- Title: ' . $note['title'] . ' (ID: ' . $note['id'] . ')',
             $notes,
         ));
+    }
+
+    /**
+     * @param string $id The ID of the note to read
+     */
+    public function read(string $id): string
+    {
+        $response = $this->nextcloudClient->request(
+            'GET',
+            '/apps/notes/api/v1/notes/' . $id,
+        );
+
+        $note = $response->toArray();
+
+        return $note['content'] ?? 'Invalid or empty note!';
     }
 }
