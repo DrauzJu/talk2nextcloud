3e80a234ed02090fd528c59c52f657777c34a022
Julian Drauz <julian@drauz-mail.de>
2025-10-03 16:09:21 +0200

    Fix biome config

--- assets/App.vue
+++ assets/App.vue
@@ -3,6 +3,5 @@
 </template>
 
 <script setup lang="ts">
-// biome-ignore lint/correctness/noUnusedImports: Component is used in the template
 import MainPage from './components/MainPage.vue';
 </script>
