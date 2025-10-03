9cddbb665969e9d50fca10113980b8e4231e98db
Julian <DrauzJu@users.noreply.github.com>
2025-10-03 16:10:01 +0200

    Fix biome config (#12)

--- assets/vue-shim.d.ts
+++ assets/vue-shim.d.ts
@@ -2,6 +2,7 @@
 
 declare module '*.vue' {
     import type { DefineComponent } from 'vue';
+    // biome-ignore lint: Needed for Vue components
     const component: DefineComponent<{}, {}, any>;
     export default component;
 }
